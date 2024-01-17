8e08e1bbe1782af5e3294f5f787cb79a94ec6959
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2024-01-17 12:20:03 -0300

    configurando conexão com a api

--- carrinho/src/pages/home/index.tsx
+++ carrinho/src/pages/home/index.tsx
@@ -1,32 +1,59 @@
+import { useEffect, useState } from "react"
+import { api } from "../../service/api"
 import { BsCartPlus } from "react-icons/bs"
-export function Home(){
-    return(
+
+interface productsProps {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    cover: string;
+}
+
+export function Home() {
+    const [products, setProducts] = useState<productsProps[]>([])
+
+    useEffect(() => {
+        async function getProducts() {
+            const response = await api.get("/products")
+            setProducts(response.data)
+        }
+
+        getProducts();
+    }, [])
+
+    return (
         <div>
-           <main className="w-full max-w-7xl px-4 mx-auto">
-            <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
-                Produtos em alta 
-            </h1>
+            <main className="w-full max-w-6xl px-4 mx-auto">
+                <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+                    Produtos em alta
+                </h1>
 
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-                <section className="w-full ">
-                    <img
-                        className="w-full rounded-lg max-h-70 mb-2"
-                        src="https://media.istockphoto.com/id/1204039347/pt/foto/apple-airpods-on-a-white-background.jpg?s=612x612&w=0&k=20&c=vVFTMoQ1WCP-HRa1vLT2JxfkEjy73ThQG7KpMzUtIT4="
-                    alt="Logo do Produto"
-                    />
-                    <p className="font-medium mt-2 mb-2">Air pod</p>
-                    <div className="flex gap-3 items-center">
-                        <strong className="text-zinc-700/100 ">R$:1000</strong>
-                        <button className="bg-zinc-900 p-1 rounded">
-                            <BsCartPlus size={20} color="#fff"/>    
-                        </button>
-                    </div>
-                    
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+                    {products.map(( product ) => (<section className="w-full ">
+                        <img
+                            key={product.id}
+                            className="w-full rounded-lg max-h-70 mb-2"
+                            src={product.cover}
+                            alt={product.title}
+                         />
+                        <p className="font-medium mt-2 mb-2">{product.title}</p>
+                        <div className="flex gap-3 items-center">
+                            <strong className="text-zinc-700/100 ">{product.price.toLocaleString("pt-BR", {
+                                style: "currency",
+                                currency:"BRL"})}
+                            </strong>
+                            <button className="bg-zinc-900 p-1 rounded">
+                                <BsCartPlus size={20} color="#fff" />
+                            </button>
+                        </div>
+                    </section>
+                    ))}
 
-                </section>
-            </div>
 
-           </main>
+                </div>
+
+            </main>
         </div>
     )
 }
